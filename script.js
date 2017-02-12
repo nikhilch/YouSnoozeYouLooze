8477e81bd5e6a545aeb97d6f15bbe9f731003b40
Jonathan Kim <jonathankim2018@u.northwestern.edu>
2017-02-12 02:46:23 -0600

    updated js, html and css

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Code for application logic goes here
+// $(document).ready(function() {
+//  $("button").click (function (){
+//  	$("#button2").hide();
+//  	});
+//  	});
+ 	
+ setInterval(function() {
+    var date = new Date();
+    var cuttime =  date.getHours() + ":" + date.getMinutes() ;
+    $('#clock-wrapper').html(cuttime);
+    
+  $(".btntosetalrm").click(function(){
+     var alRmTime = $(".getalarmtime").val();  
+     if(alRmTime){
+     $('.aumkar').html(alRmTime);
+     $(".alrm-main").css("display","none");
+     $(".when-alrm-ring").css("display","block");
+     }
+        
+     else{
+     return false;
+     }
+     });
+          var awesome = $(".aumkar").html();  
+        
+    if(awesome == cuttime){
+    
+    }
+    
+}, 500);
+
+   

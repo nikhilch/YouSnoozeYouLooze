7a77591ddb4e91489eaece8d127955d5c4fd81a6
Nikhil <nikhofthehil@gmail.com>
2017-02-12 22:58:57 -0600

    Cleaning and simplifying code

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-// Code for application logic goes here
-// $(document).ready(function() {
-//  $("button").click (function (){
-//  	$("#button2").hide();
-//  	});
-//  	});
- 	
- setInterval(function() {
-    var date = new Date();
-    var cuttime =  date.getHours() + ":" + date.getMinutes() ;
-    $('#clock-wrapper').html(cuttime);
-    
-  $(".btntosetalrm").click(function(){
-     var alRmTime = $(".getalarmtime").val();  
-     if(alRmTime){
-     $('.aumkar').html(alRmTime);
-     $(".alrm-main").css("display","none");
-     $(".when-alrm-ring").css("display","block");
-     }
-        
-     else{
-     return false;
-     }
-     });
-          var awesome = $(".aumkar").html();  
-        
-    if(awesome == cuttime){
-    
-    }
-    
-}, 500);
+var timeView = document.getElementById("timeView");
+var alarmsView = document.getElementById("alarmsView");
+var statsView = document.getElementById("statsView");
 
-   
+function onTimeClicked()
+{
+    timeView.style.display = "inherit";
+    alarmsView.style.display = "none";
+    statsView.style.display = "none";
+};
+
+function onAlarmsClicked()
+{
+    timeView.style.display = "none";
+    alarmsView.style.display = "inherit";
+    statsView.style.display = "none";
+};
+
+function onStatsClicked()
+{
+    timeView.style.display = "none";
+    alarmsView.style.display = "none";
+    statsView.style.display = "inherit";
+};
 
 function ringAlarm()
 {
